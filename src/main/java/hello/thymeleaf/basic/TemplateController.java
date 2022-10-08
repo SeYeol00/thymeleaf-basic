d8029b49ddebdab4291da65875dc9b6845fe5eb9
WE-DA-pluggg <79959576+SeYeol00@users.noreply.github.com>
2022-10-08 21:35:01 +0900

    Update TemplateController.java

--- src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -1,14 +1,15 @@
 package hello.thymeleaf.basic;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Slf4j
 @Controller
 @RequestMapping("/template")
 public class TemplateController {
-
+    // 커밋 테스트 중입니다.
     @GetMapping("/fragment")
     public String template(){
         return "template/fragment/fragmentMain";
